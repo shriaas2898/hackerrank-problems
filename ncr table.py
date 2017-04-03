480a0ab46b1a15ae00640b6ba08b7df6aa23202e
shriaas2898 <aasthashrivastava410@gmail.com>
2017-04-03 10:25:00 +0530

    added link of question

--- ncr table.py
+++ ncr table.py
@@ -1,15 +1,16 @@
-def ncr(n): #n=4
+link of question-https://www.hackerrank.com/challenges/ncr-table
+def ncr(n):
     
     result=1
     for i in range(n//2):
         print(result%10**9,end=" ") 
-        result=(result*(n-i))//(i+1) #result=(3)//2
+        result=(result*(n-i))//(i+1)
     print(result%10**9,end=" ")        
     if(n%2==1 and n>1):
         print(result%10**9,end=" ")
     for j in range(n//2-1,0,-1):
-        #print("bla bla bal")
-        result=(result*(j+1))//(n-j)#//(i+1)
+        
+        result=(result*(j+1))//(n-j)
         print(result%10**9,end=" ")
     print(1)
 n=int(input("enter"))
